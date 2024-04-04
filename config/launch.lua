260d65a06b4e5c0f65de0d428c0b1b1ab8970399
atraholt <audun@traholt.net>
2024-04-04 16:43:10 +0200

    refactor: change username to audun

--- config/launch.lua
+++ config/launch.lua
@@ -8,29 +8,29 @@ local options = {
 if platform.is_win then
    options.default_prog = { 'pwsh' }
    options.launch_menu = {
-      { label = 'PowerShell Core', args = { 'pwsh' } },
+      { label = 'PowerShell Core',    args = { 'pwsh' } },
       { label = 'PowerShell Desktop', args = { 'powershell' } },
-      { label = 'Command Prompt', args = { 'cmd' } },
-      { label = 'Nushell', args = { 'nu' } },
+      { label = 'Command Prompt',     args = { 'cmd' } },
+      { label = 'Nushell',            args = { 'nu' } },
       {
          label = 'Git Bash',
-         args = { 'C:\\Users\\kevin\\scoop\\apps\\git\\current\\bin\\bash.exe' },
+         args = { 'C:\\Users\\audun\\scoop\\apps\\git\\current\\bin\\bash.exe' },
       },
    }
 elseif platform.is_mac then
    options.default_prog = { '/opt/homebrew/bin/fish' }
    options.launch_menu = {
-      { label = 'Bash', args = { 'bash' } },
-      { label = 'Fish', args = { '/opt/homebrew/bin/fish' } },
+      { label = 'Bash',    args = { 'bash' } },
+      { label = 'Fish',    args = { '/opt/homebrew/bin/fish' } },
       { label = 'Nushell', args = { '/opt/homebrew/bin/nu' } },
-      { label = 'Zsh', args = { 'zsh' } },
+      { label = 'Zsh',     args = { 'zsh' } },
    }
 elseif platform.is_linux then
    options.default_prog = { 'fish' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash' } },
       { label = 'Fish', args = { 'fish' } },
-      { label = 'Zsh', args = { 'zsh' } },
+      { label = 'Zsh',  args = { 'zsh' } },
    }
 end
 
