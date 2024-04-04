480ad0cc7b7d21ef6bee106493327807525d506c
atraholt <audun@traholt.net>
2024-04-04 16:49:04 +0200

    refactor: we need a font with Japanese support

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,8 +1,8 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
-local font = 'JetBrainsMono Nerd Font'
-local font_size = platform().is_mac and 12 or 9
+local font = 'UDEV Gothic 35NFLG'
+local font_size = platform().is_mac and 12 or 11
 
 return {
    font = wezterm.font(font),
