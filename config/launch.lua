b3c810502c9b7fc763b67677124ad604466f7aa5
Audun Tråholt <audun@traholt.net>
2024-11-29 04:18:42 +0100

    kill nushell and fish

--- config/launch.lua
+++ config/launch.lua
@@ -11,25 +11,21 @@ if platform.is_win then
       { label = 'PowerShell Core',    args = { 'pwsh', '-NoLogo' } },
       { label = 'PowerShell Desktop', args = { 'powershell' } },
       { label = 'Command Prompt',     args = { 'cmd' } },
-      { label = 'Nushell',            args = { 'nu' } },
       {
          label = 'Git Bash',
          args = { 'C:\\Users\\audun\\scoop\\apps\\git\\current\\bin\\bash.exe' },
       },
    }
 elseif platform.is_mac then
-   options.default_prog = { '/opt/homebrew/bin/fish', '-l' }
-   options.launch_menu = {
-      { label = 'Bash',    args = { 'bash', '-l' } },
-      { label = 'Fish',    args = { '/opt/homebrew/bin/fish', '-l' } },
-      { label = 'Nushell', args = { '/opt/homebrew/bin/nu', '-l' } },
-      { label = 'Zsh',     args = { 'zsh', '-l' } },
-   }
-elseif platform.is_linux then
-   options.default_prog = { 'fish', '-l' }
+   options.default_prog = { '/opt/homebrew/bin/zsh', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { 'fish', '-l' } },
+      { label = 'Zsh',  args = { '/opt/homebrew/bin/zsh', '-l' } },
+   }
+elseif platform.is_linux then
+   options.default_prog = { 'zsh', '-l' }
+   options.launch_menu = {
+      { label = 'Bash', args = { 'bash', '-l' } },
       { label = 'Zsh',  args = { 'zsh', '-l' } },
    }
 end
